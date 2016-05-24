a6c6af6b0079d798626dfb42b04637eb2c5be309
Alejandro Carrau <acarrau@users.noreply.github.com>
2016-05-24 12:29:38 -0300

    Add state

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text',
+      category: 0
+    }
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
   render(){
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+    let propsTxt = this.props.txt
+    return <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>State text: {this.state.txt} </h1>
+        <h1>Props text: {propsTxt}</h1>
+      </div>
   }
 }
 
 App.propTypes = {
   txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
+  category: React.PropTypes.number.isRequired
 }
 
 App.defaultProps = {
-  txt: 'this is the default txt'
+  txt: 'this is the default props text'
 }
 
 ReactDOM.render(
-  <App cat={5} txt="this is the props text" />,
+  <App category={5} txt="this is the props text" />,
   document.getElementById('app')
 );
